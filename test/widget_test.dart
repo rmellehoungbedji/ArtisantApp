9f7659c5e6c2da846935a3eb1edbd73b08465032
josiane-commits <agbomassatinj@gmail.com>
2020-01-24 18:56:40 +0100

    modification

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:alonouz_mobile/main.dart';
+import '../lib/myApp.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
